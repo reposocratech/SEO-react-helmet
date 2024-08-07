111f5911ec59301bef8aedc68b71c214c32a8c72
sergio jimenez <sjr.social@gmail.com>
2024-08-07 14:23:13 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/reposocratech/SEO-react-helmet",
+  base: "https://reposocratech.github.io/SEO-react-helmet/",
 });
